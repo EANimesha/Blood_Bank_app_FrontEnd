ffd56829c78b3654b2a454ceaaed984f73d6157b
EANimesha <nimeshadilini999@gmail.com>
2019-11-16 01:51:46 +0530

    add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { AuthenticationService } from './../authentication.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup= new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    password:  new FormControl(null, Validators.required)
+  });
+
+  constructor(private _router: Router, private _auth: AuthenticationService) { }
+
+  moveToRegister(){
+    this._router.navigate(['/register']);
+  }
+  ngOnInit() {
+  }
+
+  login(){
+    if (!this.loginForm.valid) {
+      console.log('invalid form');
+      return;
+    }
+    // console.log(JSON.stringify(this.loginForm.value));
+    this._auth.login(JSON.stringify(this.loginForm.value))
+    .subscribe(
+      data => {console.log(data); this._router.navigate(['/user']); } ,
+      error => console.error(error)
+    );
+  }
+
+}
