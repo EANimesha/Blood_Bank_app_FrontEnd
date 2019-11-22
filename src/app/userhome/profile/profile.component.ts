66987ccd2397e76a5a20a600198ca2ba54f796ae
EANimesha <nimeshadilini999@gmail.com>
2019-11-22 07:21:05 +0530

    added a new component to update the user profile data

---
+++ src/app/userhome/profile/profile.component.ts
@@ -0,0 +1,57 @@
+import { User } from './../../models/User';
+import { ProfileService } from './../../profile.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/authentication.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  id;
+  user: User;
+
+  updateForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    first_name: new FormControl(null, Validators.required),
+    last_name: new FormControl(null, Validators.required),
+    password:  new FormControl(null, Validators.required),
+    donar: new FormControl(null, Validators.required),
+    blood_type: new FormControl(),
+    weight: new FormControl(),
+    height : new FormControl(),
+  });
+
+  constructor(private route: ActivatedRoute, private _profile: ProfileService , private _auth: AuthenticationService, fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      // console.log(this.id)
+      this._auth.getUser(this.id).subscribe((res: any) => {
+        this.user = res[0];
+        // console.log(this.user);
+        this.updateForm.patchValue(this.user);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  update(){
+    if(!this.updateForm.valid){
+      console.log('invalid form');
+      return;
+    }
+    this._profile.update(JSON.stringify(this.updateForm.value), this.id)
+    .subscribe(
+      data => {console.log(data); },
+      error => console.error(error)
+    );
+    // console.log(JSON.stringify(this.updateForm.value));
+  }
+
+}
