e3f8af21c38a75d293887765403cdb57b4c1be0d
EANimesha <nimeshadilini999@gmail.com>
2019-11-30 22:41:52 +0530

    Added jwt token passing and saving codes

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,6 +1,7 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders , HttpResponse } from '@angular/common/http';
-import {User} from './models/User';
+import { User, UserDetails } from './models/User';
 import { map } from 'rxjs/operators';
 
 export class LoginUser {
@@ -12,8 +13,50 @@ export class LoginUser {
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private token: string;
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient,private _rout: Router) { }
+
+  public saveToken(token: string): void {
+    localStorage.setItem('mean-token', token);
+    this.token = token;
+  }
+
+  private getToken(): string {
+    if (!this.token) {
+      this.token = localStorage.getItem('mean-token');
+    }
+    return this.token;
+  }
+  public logout(): void {
+    this.token = '';
+    window.localStorage.removeItem('mean-token');
+    this._rout.navigateByUrl('/');
+  }
+
+  public getUserDetails(): UserDetails {
+    const token = this.getToken();
+    let payload;
+    if (token) {
+      payload = token.split('.')[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload);
+    } else {
+      return null;
+    }
+  }
+
+  public isLoggedIn(): boolean {
+    const user = this.getUserDetails();
+    if (user) {
+      return user.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+
+//////////////////////////////////////////////////////////////
+
 
   register(body: any) {
     return this._http.post('http://127.0.0.1:3000/viewers/register',body,{
@@ -29,9 +72,9 @@ export class AuthenticationService {
     });
   }
 
-  getUser(id:any){
-    const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.get('http://127.0.0.1:3000/viewers/'+ id,{headers});
+  getUser(id: any){
+    const headers = new HttpHeaders({Authorization: `Bearer ${this.getToken()}`}).set('Content-Type', 'application/json');
+    return this._http.get('http://127.0.0.1:3000/viewers/user',{headers});
   }
 
   getDonars(){
